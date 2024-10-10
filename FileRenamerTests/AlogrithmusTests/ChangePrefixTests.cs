acf24f3cbb30a501ce6d385acb72f4a4e5e2cc93
Leon Harrer <Harrer@gtue.de>
2024-10-10 15:09:22 +0200

    Zweiter Test (eine Datei bereits umbenannt

--- FileRenamerTests/AlogrithmusTests/ChangePrefixTests.cs
+++ FileRenamerTests/AlogrithmusTests/ChangePrefixTests.cs
@@ -41,5 +41,31 @@ namespace FileRenamerTests.AlogrithmusTests
                 It.Is<string>(dest => dest == @"C:\Test\003-test.jpg")
             ), Times.Once);
         }
+
+        [TestMethod]
+        public void ChangePrefixOneAlreadyCorrect()
+        {
+            var files = new List<string>
+            {
+                @"C:\Test\001-test.jpg",
+                @"C:\Test\file2.jpg"
+            };
+            var newNamePattern = "test";
+            var directoryPath = @"C:\Test";
+            var fileMoverMock = new Mock<IFileMover>();
+            var algorithmus = new Algorithmus(fileMoverMock.Object);
+
+            algorithmus.ChangePrefix(files, newNamePattern, directoryPath);
+
+            fileMoverMock.Verify(f => f.Move(
+                It.Is<string>(src => src == @"C:\Test\001-test.jpg"),
+                It.IsAny<string>()
+            ), Times.Once);
+
+            fileMoverMock.Verify(f => f.Move(
+                It.Is<string>(src => src == @"C:\Test\file2.jpg"),
+                It.Is<string>(dest => dest == @"C:\Test\002-test.jpg")
+            ), Times.Once);
+        }
     }
 }
