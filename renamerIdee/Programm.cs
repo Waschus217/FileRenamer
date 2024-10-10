38a57205e82ca63107dca317d7ddcb56e2ed800e
Leon Harrer <Harrer@gtue.de>
2024-10-10 15:11:19 +0200

    Aufgeräumt

---
+++ renamerIdee/Programm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using renamerIdee.Interfaces;
+
+namespace renamerIdee
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            var fileMover = new FileMover();
+            Algorithmus algorithmus = new Algorithmus(fileMover);
+            algorithmus.AlgorithmRenamePictureFiles();
+        }
+    }
+}
